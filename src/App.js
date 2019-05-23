0c4c4300c30027292a5fe89a9113cceb945b8ea5
Mustafa Saifee <saifee.mustafa@gmail.com>
2019-05-23 09:27:47 +0300

    Updating data

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="App">
       <div className="stickyinfo">
-        2019 live counting. Last Updated 11:37 AM IST. 2019 data is showing lead and not wins. The sex and SC/ST winner and turnout data is unavailable.
+        2019 live counting. Last Updated 11:52 AM IST. 2019 data is showing lead and not wins. The sex and SC/ST winner and turnout data is unavailable.
       </div>
       <div className="head">
         <div className="headImg" >
